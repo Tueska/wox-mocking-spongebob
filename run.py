ecefab5e0d3b31c63123f976afe977f882508a89
Tueska <36133860+Tueska@users.noreply.github.com>
2020-04-15 13:58:12 +0200

    Add files via upload

---
+++ run.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import os
+from wox import Wox
+
+class Mock(Wox):
+
+    def copyToClipboard(self, value):
+        os.system('echo | set /p nul=' + value.strip() + '| clip')
+
+    def query(self, query):
+        results = []
+        irony = ""
+        i = False
+        for char in query:
+            if i:
+                irony += char.upper()
+            else:
+                irony += char.lower()
+            if char != ' ':
+                i = not i
+
+        results.append({
+            "Title": irony,
+            "SubTitle": "Original: \"{}\"".format(query),
+            "IcoPath":"irony.png",
+            "JsonRPCAction":{
+                "method": "copyToClipboard",
+                "parameters":[irony],
+                "dontHideAfterAction":False
+            }
+        })
+
+
+        return results
+
+if __name__ == "__main__":
+    Mock()
